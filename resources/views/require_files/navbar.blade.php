5fd9984f19f8603c522f3cb258b25a3211b72da4
Unknown <derekstill2000@gmail.com>
2018-10-05 10:55:56 +0200

    navbar toegevoegd

---
+++ resources/views/require_files/navbar.blade.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html class="no-js" lang="">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="main.css">
+</head>
+<body>
+<nav>
+    <h1><b>BARROC IT</b><br>Software for real</h1>
+
+    <h2>Finance</h2>
+    <div class="tooltip"><h2>?</h2>
+        <span class="tooltiptext">Tooltip text</span>
+    </div>
+    </div>
+    <div class="navbuttons">
+        <button class="navbutton"><span>Schedule Appointment</span></button>
+        <button class="navbutton"><span>Client Information</span></button>
+        <button class="navbutton"><span>Create Client</span></button>
+    </div>
+
+
+</nav>
+<div class="main">
+
+</div>
+
+
+
+<script src="js/vendor/modernizr-3.5.0.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+<script>
+    window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+    ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+</script>
+<script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+</html>
