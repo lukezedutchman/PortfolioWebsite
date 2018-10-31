768e749a8868f8956ab6d553c6b1ea86e4bba368
lukezedutchman <specularsteam28@outlook.com>
2018-10-31 15:05:53 +0100

    register controller nog niet opgelost

--- app/User.php
+++ app/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'IsUser',
+        'name', 'email', 'IsUser', 'password',
     ];
 
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -19,8 +19,10 @@
                     <div class="col-md-8">
                         <div class="card">
 
+                            <div class="card-header">{{ __('Register') }}</div>
+
                             <div class="card-body">
-                                <form method="POST" action="{{ route('registerAccount', ['IsUser' => '1']) }}">
+                                <form method="POST" action="{{ route('register') }}">
                                     @csrf
 
                                     <div class="form-group row">
